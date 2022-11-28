eabe8a466e83b947577998773e9069ce6cc474a4
esi <esi@gmail.com>
2022-11-28 22:03:11 +0330

    add todo project

---
+++ lib/data.dart
@@ -0,0 +1,16 @@
+import 'package:hive_flutter/adapters.dart';
+part 'data.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject{
+  @HiveField(0)
+  String name = '';
+  @HiveField(1)
+  bool isCompleted = false;
+  @HiveField(2)
+  Priority priority = Priority.low;
+
+}
+enum Priority{
+  low,normal,high
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo/data.dart';
+
+const taskBoxName = 'tasks';
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  Hive.openBox<Task>(taskBoxName);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen()
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final box = Hive.box<Task>(taskBoxName);
+    return Scaffold(
+      appBar: AppBar(title: Text('To Do List'),),
+      floatingActionButton: FloatingActionButton.extended(onPressed: () {
+       Navigator.of(context).push(MaterialPageRoute(builder: (context) => EditTaskScreen(),));
+      }, label: Text('Add New Task')),
+      body: ListView.builder(
+        itemCount: box.values.length,
+        itemBuilder: (context, index) {
+          final Task task = box.values.toList()[index];
+          return Container(
+           
+          );
+      },),
+    );
+  }
+}
+
+class EditTaskScreen extends StatelessWidget {
+  EditTaskScreen({Key? key}) : super(key: key);
+ TextEditingController _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Edit Task'),),
+      floatingActionButton: FloatingActionButton.extended(onPressed: () {
+        final task = Task();
+        task.name = _controller.text;
+        task.priority = Priority.low;
+        task.save();
+        Navigator.of(context).pop();
+      }, label: Text('Save Changes')),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              label: Text('Add a task for today...')
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
